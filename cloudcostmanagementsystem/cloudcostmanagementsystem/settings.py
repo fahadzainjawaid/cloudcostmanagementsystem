e385c3e6df2f73a7218dd930bf2ca1f8e4775e85
Fahad Zain Jawaid <Fahad.jawaid@utoronto.ca>
2023-04-04 21:44:59 -0400

    separate out secret key and add to git ignore

--- cloudcostmanagementsystem/cloudcostmanagementsystem/settings.py
+++ cloudcostmanagementsystem/cloudcostmanagementsystem/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lw(*ud68ms01pax8$m2iy(xcr7myyq2^ds=z6q7piibkssvg*m'
+SECRET_KEY = secret_key.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
